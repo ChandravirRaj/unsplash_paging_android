52db4878d541c7d655800bd2b16e0c8f8474473b
chandravir <chandravir.singh@mobcoder.com>
2024-04-15 16:37:41 +0530

    create folder structure

--- app/src/main/java/com/androboy/unsplashpaging/ui/MainActivity.kt
+++ app/src/main/java/com/androboy/unsplashpaging/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.androboy.unsplashpaging
+package com.androboy.unsplashpaging.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.androboy.unsplashpaging.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
