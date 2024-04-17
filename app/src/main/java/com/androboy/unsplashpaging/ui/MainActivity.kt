ba2f9d9fed4fa331508a11b90bebcdd15a87f24b
chandravir <chandravir.singh@mobcoder.com>
2024-04-17 11:08:45 +0530

    manage image height in grid view

--- app/src/main/java/com/androboy/unsplashpaging/ui/MainActivity.kt
+++ app/src/main/java/com/androboy/unsplashpaging/ui/MainActivity.kt
@@ -3,13 +3,15 @@ package com.androboy.unsplashpaging.ui
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.paging.PagingData
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import androidx.viewbinding.ViewBinding
 import com.androboy.unsplashpaging.databinding.ActivityMainBinding
 import com.androboy.unsplashpaging.ui.adapters.LoaderAdapter
 import com.androboy.unsplashpaging.ui.adapters.PhotosPagingAdapter
 import com.androboy.unsplashpaging.ui.model.UnsplashPhoto
 import com.androboy.unsplashpaging.ui.viewmodel.MainViewModel
+import com.androboy.unsplashpaging.utils.SpacingItemDecorator
 import com.androboy.unsplashpaging.utils.imgloading.ImageLoader
 import com.androboy.unsplashpaging.utils.invisible
 import com.androboy.unsplashpaging.utils.visible
@@ -45,16 +47,20 @@ class MainActivity : BaseActivity() {
 
         ui.progressBar.visible()
         photoPagingAdapter = PhotosPagingAdapter()
-        ui.recyclerView.layoutManager = GridLayoutManager(this,2)
+        val staggeredGridLayoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        staggeredGridLayoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS;
+        ui.recyclerView.layoutManager = staggeredGridLayoutManager
         ui.recyclerView.setHasFixedSize(true)
         ui.recyclerView.adapter = photoPagingAdapter.withLoadStateHeaderAndFooter(
             header = LoaderAdapter(), footer = LoaderAdapter()
         )
+        val x = (resources.displayMetrics.density * 1).toInt()
+        ui.recyclerView.addItemDecoration(SpacingItemDecorator(x))
 
         imgLoader = ImageLoader(this)
         photoPagingAdapter.setImageLoader(imgLoader)
 
-        GlobalScope.launch { // context of the parent, main runBlocking coroutine
+        GlobalScope.launch {
             delay(2000)
             ui.progressBar.invisible()
         }
